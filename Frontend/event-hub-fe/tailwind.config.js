bd3d647555a51615e1c4a84042cb5b04ba95a1b7
Artem Z. <107121930+ArtemUnreal71@users.noreply.github.com>
2025-07-26 14:03:41 +0200

    feat: Packages & Styling
    Added necessary packages and base styling (colors and font)

--- Frontend/event-hub-fe/tailwind.config.js
+++ Frontend/event-hub-fe/tailwind.config.js
@@ -1,8 +1,28 @@
 /** @type {import('tailwindcss').Config} **/
 export default {
   content: ["./index.html", "./src/**/*.{js,ts,jsx,tsx}"],
+  darkMode: "class",
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        primary: "var(--color-primary)",
+        primaryDark: "var(--color-primary-dark)",
+        primaryLight: "var(--color-primary-light)",
+        background: "var(--color-background)",
+        surface: "var(--color-surface)",
+        onSurface: "var(--color-on-surface)",
+        text: "var(--color-text)",
+        textMuted: "var(--color-text-muted)",
+        buttonHover: "var(--color-button-hover)",
+        accent: "var(--color-accent)",
+        icon: "var(--color-icon)",
+        border: "var(--color-border)",
+        card: "var(--color-card)",
+      },
+      fontFamily: {
+        hub: ['"Inter"', "sans-serif"],
+      },
+    },
   },
   plugins: [],
 };
